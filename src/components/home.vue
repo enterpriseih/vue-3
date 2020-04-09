7c16ddc8bcc945d368310e6aa24319e5acc61eff
shenya.ji.o <shenya.ji.o@nio.com>
2020-04-09 14:02:50 +0800

    update

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="page flex-center">
-        <div class="detail-card">
+    <div class="flex-center" style="width: 100%;">
+        <div class="detail_card flex-center" >
+            <el-row class="detail_content" type="flex" justify="space-around">
+                <el-col :span="16">
+                    <el-row>
+                        <el-card shadow="hover">
+                            <h5>文章标题</h5>
+                            <el-row></el-row>
+                        </el-card>
+                    </el-row>
+                </el-col>
+                <el-col :span="6">
+                    <el-row>
+                        <el-card shadow="hover">
+                            <h5>标签</h5>
+                        </el-card>
+                    </el-row>
+                </el-col>
+            </el-row>
         </div>
     </div>
 </template>
@@ -11,14 +28,45 @@
         components: {},
         data () {
             return {}
+        },
+        mounted () {
+            var name = 'the window'
+            var object = {
+                name: 'my object',
+                getNameFunc: function () {
+                    console.log(this.name)
+                    return function () {
+                        return this.name
+                    }
+                }
+            }
+            console.log(object.getNameFunc())
+            var name1 = 'the window1'
+            var object1 = {
+                name: 'my object1',
+                getNameFunc: function () {
+                    var that = this
+                    return function () {
+                        return that.name
+                    }
+                }
+            }
+            console.log(object1.getNameFunc()())
         }
     }
 </script>
 
 <style scoped lang="stylus">
-    .detail-card
-        width 600px
-        height 300px
+    .detail_card
+        width 80%
         margin-top 40px
+        flex-direction column
+    .detail_content
+        width 100%
+    .detail_left
+        width 60%
+        height 250px
         background-color white
+    .detail_right
+        background-color black
 </style>
