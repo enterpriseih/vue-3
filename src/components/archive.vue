52fd563af1c2d189d90e95a9d87f3cf5836e3f68
shenya.ji.o <shenya.ji.o@nio.com>
2020-05-27 16:42:25 +0800

    1

--- src/components/archive.vue
+++ src/components/archive.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>归档</div>
+    <div class="flex-center" style="width: 100%;">
+        <div class="detail_card flex-center" >
+            <el-row class="detail_content" type="flex" justify="space-around">
+                <el-col :span="16">
+                    <el-row>
+                        <el-card shadow="hover">
+                            <h5>归档</h5>
+                            <el-row></el-row>
+                        </el-card>
+                    </el-row>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +21,17 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+    .detail_card
+        width 80%
+        margin-top 40px
+        flex-direction column
+    .detail_content
+        width 100%
+    .detail_left
+        width 60%
+        height 250px
+        background-color white
+    .detail_right
+        background-color black
 </style>
